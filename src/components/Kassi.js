ea91979b57ec65c4460940c94173f467a93e1fae
adamelid <adamelid16@gmail.com>
2023-05-11 12:07:01 +0000

    Update Kassi.js

--- src/components/Kassi.js
+++ src/components/Kassi.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Kassi(props) {
+    const { slug } = useParams()
     return (
-    <button className="btn" onClick={props.onClick}>{props.value}</button>
+    <button className="btn" onClick={props.onClick}
+        style={{backgroundImage: `url('../images/${slug}.jpg')`}}>
+        {props.value}
+    </button>
     );
 }
